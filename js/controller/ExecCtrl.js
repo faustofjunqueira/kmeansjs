3d6d7f7f8d0fbcce3508fc48b8941c090cb9ff07
Fausto Junqueira <faustoferjunqueira@gmail.com>
2014-11-04 21:54:49 -0200

    CSVService criado, e leitura de arquivo

--- js/controller/ExecCtrl.js
+++ js/controller/ExecCtrl.js
@@ -1,5 +1,17 @@
 angular.module('kmeans')
 
-.controller('ExecCtrl',['$scope',function($scope){
+.controller('ExecCtrl',['$scope','CSVService',function($scope,CSVService){
+	
+	$scope.formInput = {epocas:0, k : 0};
+	var Pontos;
+
+	$scope.mostraConteudo = function($fileContent){
+    	$scope.ConteudoCSV = $fileContent;
+    	console.log($scope.ConteudoCSV);
+	};
+	
+	$scope.executar = function(){
+		Pontos = CSVService.textoParaMatriz($scope.ConteudoCSV);
+	};
 
 }]);
